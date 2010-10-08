04eed106ada74411e52fc22afaef3ba7874afecf
Brett Buddin <brett@intraspirit.net>
2010-10-08 10:40:38 -0400

    Extra test to validate that we're implementing the engine correctly

--- spec/static_pages_spec.rb
+++ spec/static_pages_spec.rb
@@ -1,7 +1,11 @@
 require 'helper'
 
 describe StaticPages do
-  it "should be valid" do
+  it "is valid" do
     StaticPages.should be_a(Module)
+  end
+
+  it "is an engine" do
+    assert StaticPages::Engine.superclass == Rails::Engine
   end
 end
